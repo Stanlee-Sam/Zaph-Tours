3e051491796c12b1d46d37fde6356b293cc1bde7
stanleyamunze03@gmail.com <stanleyamunze03@gmail.com>
2024-06-06 08:33:23 +0300

    deployment changes

--- src/components/Home page/Newsletter/newsletter.jsx
+++ src/components/Home page/Newsletter/newsletter.jsx
@@ -6,10 +6,12 @@ const Newsletter = () => {
     <section className='newsletter-section'>
        <div className="card-content">
        <h1>Newsletter</h1>
+        <p>Are you a thrill-seeker? On the lookout for your next adventure? Get on board Zaph Tours’ rollicking rollercoaster of weekly updates on safaris and beyond. Buckle up and brace yourself for the ride of your life!</p>
+       
         <p>Subscribe to our newsletter and get 20% off</p>
         <form className='newsletter-form'>
             <input type="text" placeholder="Enter your email address" />
-            <button type="submit">Subscribe</button>
+            <button type="button">Subscribe</button>
         </form>
        </div>
         
